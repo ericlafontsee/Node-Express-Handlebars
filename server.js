0f0808f58843f8aa1b868ccb1d9b427de6832704
Eric LaFontsee <elafontsee@gmail.com>
2020-09-23 19:22:05 -0500

    Setup server.js with Express app so the data can be parsed, handlebars is setup, and a listener is placed on PORT

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 var express = require("express");
-var connection = require("config/connection.js");
+var exphbs = require("express-handlebars");
 var mysql = require("mysql");
+var routes = require("./controllers/burger_controller.js");
 
+var app = express();
 var PORT = process.env.PORT || 8080;
 
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
+app.use(express.static("public"));
 
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use("routes");
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
